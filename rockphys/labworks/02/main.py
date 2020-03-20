50ff7f977fa5989da4e0d403447b93a172d4c513
Datasciensyash <60406311+Datasciensyash@users.noreply.github.com>
2020-03-20 21:16:32 +0300

    Vtoraya laba

--- rockphys/labworks/02/main.py
+++ rockphys/labworks/02/main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-from modules import get_coeffs, get_K
+from modules import get_coeffs, get_K, arr_wa
 import pandas as pd
 import cv2
 
@@ -12,7 +12,7 @@ P_values = st.text_input('Values of P', value='65, 72, 75')
 P_values = np.array(eval('[' + P_values + ']')) #That is bad.
 
 H_values = st.text_input('Values of H', value='65, 72, 75')
-H_values = np.array(eval('[' + H_values + ']')) #That is bad.
+H_values = np.array(eval('[' + H_values.replace(' ', ',') + ']')) #That is bad.
 
 K_values = []
 for i in range(len(A_values)):
@@ -28,43 +28,60 @@ df = pd.DataFrame(list(zip(A_values, P_values, H_values, K_values, KM_values)), 
 st.table(df)
 filename = st.text_input('Csv filename', value='output.csv')
 if st.button('Save'):
-    #grid = []
-    #for i in range(7):
-        #for j in range(15):
-            #grid.append([i, j, KM_values[i * 7 + j]])
-    #print(grid)
-    #pd.DataFrame(grid).to_csv(f'grid-{filename}', index=False)
     pd.DataFrame(KM_values.reshape(15, 7), index='1 2 3 4 5 6 7 8 9 10 11 12 13 14 15'.split(' '), columns='I II III IV V VI VII'.split(' ')).to_csv(f'grid-{filename}', index=False)
     df.to_csv(filename, index=False)
 
 
 try:
     plt.figure(figsize=(8, 8))
-    image = cv2.resize(KM_values.reshape(7, 15).astype('float'), (3000, 3500), interpolation=cv2.INTER_CUBIC)
-    for i in range(0, int(image.max() // 100) + 1):
-        image = np.where((image < (100 * (i + 1))) & (image > (100 * i)), i, image)
+    arr = KM_values.reshape(7, 15).astype('float').T
+    n = st.slider('n', min_value=1, max_value=25, value=1)
+    det = st.slider('Denominator', min_value=0.1, max_value=1.0, value=0.1)
+    arr1 = cv2.resize(arr, (4 , 8), interpolation=cv2.INTER_CUBIC).astype(np.float64)
+    arr2 = cv2.resize(arr, (4 , 6), interpolation=cv2.INTER_CUBIC).astype(np.float64)
+    #Priors_start
+    arr1[0:7, 3] = arr1[0:7, 3] * det
+    arr1[7, 0:3] = arr1[7, 0:3] * det
 
+    arr2[0:5, 3] = arr2[0:5, 3] * det
+    arr2[5, 0:3] = arr2[5, 0:3] * det
 
+    arr1[7, 3] = arr1[7, 3] * det
+    arr2[5, 3] = arr2[5, 3] * det   
 
+    #Priors_end        
+    arr1 = cv2.resize(arr1, (7 * 2 * n , 15 * n), interpolation=cv2.INTER_CUBIC)
+    arr2 = cv2.resize(arr2, (7 * 2 * n , 15 * n), interpolation=cv2.INTER_CUBIC)   
+    arr = (arr1 + arr2) / 2
 
-    plt.imshow(image.astype(np.uint8))
-    plt.colorbar(ticks=range(7), label='sex')
-    st.pyplot()
+    image = arr
+    for i in range(int(image.min() // 100), int(image.max() // 100) + 1):
+        image = np.where((image < (100 * (i + 1))) & (image > (100* i)), i, image)
 
-    def discrete_matshow(data):
+    def discrete_matshow(data, n):
+        fig, ax = plt.subplots(figsize=(8, 8))
         #get discrete colormap
         cmap = plt.get_cmap('viridis', np.max(data)-np.min(data)+1)
         # set limits .5 outside true range
-        mat = plt.matshow(data, cmap=cmap,vmin = np.min(data)-.5, vmax = np.max(data)+.5)
+        mat = ax.matshow(data, cmap=cmap, vmin = np.min(data) - 0.5, vmax = np.max(data) + 0.5)
         #tell the colorbar to tick at integers
         cax = plt.colorbar(mat, ticks=np.arange(np.min(data), np.max(data) + 1))
-        cax.ax.set_yticklabels([f'{i * 100}' for i in range(int(np.max(data)) + 1)])
-        cax.ax.set_title(r'$К_{m}$ $\frac {м^2}{сут}$', fontsize=12)
+        cax.ax.set_yticklabels([f'{i * 100}-{(i + 1) * 100}' for i in range(int(np.max(data)) + 1)])
+        cax.ax.set_title(r'$К_{m}$ $\frac {м^2}{сут}$', fontsize=25)
 
-    discrete_matshow(image.astype(np.uint8))
+        picket_size = n
+        profile_size = n * 2
+        plt.yticks([picket_size * i for i in range(0, 15)], [i + 1 for i in range(0, 15)], rotation=0, size=7)
+        plt.xticks([profile_size * i for i in range(0, 7)], [i + 1 for i in range(0, 7)], rotation=0, size=7)
+        ax.tick_params(axis="x", bottom=True, top=False, labelbottom=True, labeltop=False)
+        plt.xlabel("Номер профиля")
+        plt.ylabel("Номер пикета")
 
-    plt.xlabel("TA")
-    plt.ylabel("DA")
+
+
+    discrete_matshow(image.astype(np.float32), n)
+
     st.pyplot()
-except:
+except Exception as e:
     st.info('Waiting for data...')
+    st.info(str(e))
--- rockphys/labworks/02/modules.py
+++ rockphys/labworks/02/modules.py
@@ -4,9 +4,23 @@ def get_coeffs(p):
 	"""
 	Works good enough for p in range(200, 50)
 	"""
-	a = 3.1843687374749496 + -0.010020040080160442 * p
+	a = 3.1843687374749496 - 0.010020040080160442 * p
 	b = -1.3392612376400044
 	return a, b
 
 def get_K(A, a, b):
 	return np.e ** (a + b * np.log(A))
+
+def arr_wa(arr, k=0.1):
+	k_matrix = np.array([
+		[k, k, k],
+		[k, 1, k],
+		[k, k, k],
+		])
+	new_arr = np.zeros(arr.shape)
+	arr = np.pad(arr, 1, mode='edge') #, constant_values=0).copy()
+	for i in range(new_arr.shape[0]):
+		for j in range(new_arr.shape[1]):
+			print(arr[i:i + 3, j:j+3].shape)
+			new_arr[i, j] = np.sum(arr[i:i + 3, j:j+3].copy() * k_matrix) / np.sum(k_matrix)#np.sum((arr[i:i + 3, j:j+3].copy() != np.zeros((3, 3))) * k_matrix)
+	return new_arr 
